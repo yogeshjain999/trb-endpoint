017ef9b307b2b523b52ad324850a7458c29ea026
Yogesh Khater <yogeshjain999@gmail.com>
2021-06-02 12:40:58 +0530

    Setup tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,148 @@
 # frozen_string_literal: true
 
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
-require "trailblazer/endpoint"
-
 require "minitest/autorun"
+
+require "trailblazer/endpoint"
+require "trailblazer"
+require "reform/form/dry"
+
+Minitest::Spec.class_eval do
+  User = Struct.new(:id, :role) do
+    def self.find_by(id:)
+      return if id == 2
+      User.new(id, "admin")
+    end
+  end
+
+  class UserForm < Reform::Form
+    feature Reform::Form::Dry
+
+    property :id
+    property :role
+
+    validation do
+      params do
+        required(:role).filled
+      end
+    end
+  end
+
+  class UserPolicy
+    def initialize(user, model)
+      @user   = user
+      @model  = model
+    end
+
+    def update?
+      @user.id == @model.id
+    end
+  end
+
+  module Operation
+    class New < Trailblazer::Operation
+      step Model(User)
+    end
+
+    class Create < Trailblazer::Operation
+      step Subprocess(New)
+      step Contract::Build(constant: UserForm)
+      step Contract::Validate(invalid_data_terminus: true)
+    end
+
+    class Update < Trailblazer::Operation
+      step Model(User, :find_by, not_found_terminus: true)
+      # DISCUSS: Accept terminus flag in Pundit ?
+      step Policy::Pundit(UserPolicy, :update?), Output(:failure) => End(:unauthorized)
+      step Contract::Build(constant: UserForm)
+      step Contract::Validate(invalid_data_terminus: true)
+    end
+  end
+
+  class ApplicationProtocol < Trailblazer::Endpoint::Protocol
+    def authenticate(ctx, controller:, domain_ctx:, **)
+      domain_ctx[:current_user] = controller.current_user
+    end
+
+    class WithAuthorization < Trailblazer::Endpoint::Protocol::WithAuthorization
+      def authenticate(ctx, controller:, domain_ctx:, **)
+        domain_ctx[:current_user] = controller.current_user
+      end
+
+      def authorize(_ctx, domain_ctx:, **)
+        domain_ctx[:current_user].role == "admin"
+      end
+    end
+  end
+
+  class ApplicationAdapter < Trailblazer::Endpoint::Adapter
+    def unauthenticated(_ctx, controller:, **)
+      controller.render(:unauthenticated)
+    end
+
+    def unauthorized(_ctx, controller:, domain_ctx:, **)
+      controller.render(:unauthorized, "#{domain_ctx[:current_user].id} isn't allowed access!")
+    end
+
+    def not_found(_ctx, controller:, **)
+      controller.render(:not_found)
+    end
+
+    def invalid_data(_ctx, controller:, domain_ctx:, **)
+      controller.render(:invalid_data, domain_ctx[:contract].errors.full_messages)
+    end
+
+    def failure(_ctx, controller:, domain_ctx:, **)
+      controller.render(:failure, domain_ctx[:error])
+    end
+
+    def success(_ctx, controller:, **)
+      controller.render(:success)
+    end
+  end
+
+  class ApplicationController
+    include Trailblazer::Endpoint
+
+    attr_accessor :current_user, :params, :status, :message
+
+    def initialize(user)
+      self.current_user = user
+      self.params       = { id: 1 }
+    end
+
+    def self.default_endpoint_adapter
+      ApplicationAdapter
+    end
+
+    def self.default_endpoint_protocol
+      ApplicationProtocol
+    end
+
+    def render(status, message = "")
+      self.status = status
+      self.message = message
+    end
+
+    def default_endpoint_ctx
+      {
+        controller: self,
+      }
+    end
+
+    def default_domain_ctx
+      {
+        params: params
+      }
+    end
+
+    def default_flow_options
+      {
+        context_options: {
+          aliases: { 'contract.default': :contract, 'policy.default': :policy },
+          container_class: Trailblazer::Context::Container::WithAliases,
+        }
+      }
+    end
+  end
+end
--- test/trailblazer/endpoint_test.rb
+++ test/trailblazer/endpoint_test.rb
@@ -2,12 +2,122 @@
 
 require "test_helper"
 
-class Trailblazer::EndpointTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Trailblazer::Endpoint::VERSION
+class Trailblazer::EndpointTest < Minitest::Spec
+  class AdminsController < ApplicationController
+    compile_endpoint(Operation::New, protocol: ApplicationProtocol::WithAuthorization).
+      output(:success) { |_ctx, controller:, model:, **| controller.render(:success, "success from DSL") }.
+      output(:unauthorized) { |_ctx, controller:, **| controller.render(:unauthorized, "unauthorized from DSL") }
+
+    compile_endpoint Operation::Create, protocol: ApplicationProtocol::WithAuthorization
+    compile_endpoint Operation::Update
+
+    def new
+      run_endpoint Operation::New
+    end
+
+    def create
+      run_endpoint Operation::Create
+    end
+
+    def update
+      run_endpoint Operation::Update
+    end
   end
 
-  def test_it_does_something_useful
-    assert false
+  let(:user)                { User.new(1, "admin") }
+  let(:controller)          { AdminsController.new(user) }
+
+  let(:invalid_user)        { User.new(2, "employee") }
+  let(:invalid_controller)  { AdminsController.new(invalid_user) }
+
+  it "new: success" do
+    signal, (ctx, _) = controller.new
+
+    assert_equal signal.inspect, "#<Trailblazer::Activity::End semantic=:pass_fast>"
+    assert_equal controller.status, :success
+    assert_equal controller.message, "success from DSL"
+    assert_equal ctx[:domain_ctx][:model], User.new
+  end
+
+  it "new: unauthorized" do
+    signal, _ = invalid_controller.new
+
+    assert_equal signal.inspect, "#<Trailblazer::Activity::End semantic=:pass_fast>"
+    assert_equal invalid_controller.status, :unauthorized
+    assert_equal invalid_controller.message, "unauthorized from DSL"
+  end
+
+  it "create: unauthenticated" do
+    controller = AdminsController.new(nil)
+    signal, _ = controller.create
+
+    assert_equal signal.inspect, "#<Trailblazer::Activity::End semantic=:pass_fast>"
+    assert_equal controller.status, :unauthenticated
+  end
+
+  it "create: unauthorized" do
+    signal, (ctx, flow_options) = invalid_controller.create
+
+    assert_equal signal.inspect, "#<Trailblazer::Activity::End semantic=:pass_fast>"
+    assert_equal invalid_controller.status, :unauthorized
+    assert_equal invalid_controller.message, "2 isn't allowed access!"
+
+    assert_nil ctx[:domain_ctx][:model]
+
+    assert_equal flow_options, {
+      context_options: {
+        aliases: { "contract.default": :contract, "policy.default": :policy },
+        container_class: Trailblazer::Context::Container::WithAliases
+      }
+    }
+  end
+
+  it "create: invalid_data" do
+    controller.params = { id: 1, role: nil }
+    signal, (ctx, _) = controller.create
+
+    assert_equal signal.inspect, "#<Trailblazer::Activity::End semantic=:pass_fast>"
+    assert_equal controller.status, :invalid_data
+    assert_equal controller.message, ["Role must be filled"]
+
+    assert_equal ctx[:domain_ctx][:model], User.new
+    assert_equal ctx[:domain_ctx][:current_user], controller.current_user
+    assert_equal ctx[:domain_ctx][:params], { id: 1, role: nil }
+    assert_equal ctx[:endpoint_ctx][:controller], controller
+    assert_equal ctx[:controller], controller
+  end
+
+  it "create: success" do
+    controller.params = { id: 1, role: "admin" }
+    signal, (ctx, _) = controller.create
+
+    assert_equal signal.inspect, "#<Trailblazer::Activity::End semantic=:pass_fast>"
+    assert_equal controller.status, :success
+
+    assert_equal ctx[:domain_ctx][:model], User.new
+    assert_equal ctx[:domain_ctx][:current_user], controller.current_user
+    assert_equal ctx[:domain_ctx][:params], { id: 1, role: "admin" }
+    assert_equal ctx[:endpoint_ctx][:controller], controller
+    assert_equal ctx[:controller], controller
+  end
+
+  it "update: not_found" do
+    controller.params = { id: 2 }
+    signal, (ctx, _) = controller.update
+    assert_equal controller.status, :not_found
+
+    assert_equal signal.inspect, "#<Trailblazer::Activity::End semantic=:pass_fast>"
+    assert_equal ctx[:domain_ctx][:params], { id: 2 }
+    assert_nil ctx[:domain_ctx][:model]
+  end
+
+  it "update: :unauthorized" do
+    controller.params = { id: 3 }
+    signal, (ctx, _) = controller.update
+    assert_equal controller.status, :unauthorized
+
+    assert_equal signal.inspect, "#<Trailblazer::Activity::End semantic=:pass_fast>"
+    assert_equal ctx[:domain_ctx][:params], { id: 3 }
+    assert_equal ctx[:domain_ctx][:model], User.new(3, "admin")
   end
 end
